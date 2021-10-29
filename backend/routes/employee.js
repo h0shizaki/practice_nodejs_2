c2e1700e85f040b8bd608335665528f17720b60d
Kitsune-fx <kant.cmd@gmail.com>
2021-10-29 09:16:32 +0700

    [🌷Second commit]

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,8 +1,10 @@
+const e = require('express');
 const express = require('express');
 const Router = express.Router();
 const dbCon = require('../connection');
 
 Router.get("/" , (req, res) =>{
+    console.log("Get");
     dbCon.query("SELECT * FROM employee" , (error, results, fields)=>{
         if(!error){
             let message = "";
@@ -17,5 +19,86 @@ Router.get("/" , (req, res) =>{
         else throw error;
     })
 });
+
+Router.post("/", (req, res)=>{
+    console.log("Post");
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let role = req.body.role;
+    let salary = req.body.salary;
+
+    if(!fname || !lname || !role || !salary){
+        return res.status(400).send({ error: true, message: "Please add data." });
+    }
+    else{
+        dbCon.query("INSERT INTO employee (firstname, lastname, role, salary) VALUES(?,?,?,?)", 
+        [fname, lname, role, salary], (error, results, field)=>{
+            if(!error){
+                return res.status(200).send({error: false, data: results, message: "Data added"});
+            }
+            else throw error;
+        })
+    }
+})
+
+Router.get('/:id', (req, res)=>{
+    console.log("Search");
+    let id = req.params.id;
+    if(!id){
+        return res.status(400).send({error: true, message:"Please enter id"})
+    }else{
+        dbCon.query("SELECT * FROM employee WHERE id = ? ", id, (error, results, field)=>{
+            if(!error){
+                let message = ''
+                if(results === undefined || results.length == 0){
+                    message = "Employee not found"
+                }else message = "Got data";
+
+                return res.status(200).send({error: false , data: results, message})
+
+            }else throw error;
+        })
+    }
+
+})
+
+Router.delete('/', (req, res)=>{
+    console.log('Delete');
+    let id = req.body.id;
+    if(!id){
+        return res.status(400).send({error: true, message:"Please enter id"})
+    }
+    else{
+        dbCon.query("DELETE FROM employee WHERE id = ?",id, (error,results,field)=>{
+            if(!error){
+                return res.status(200).send({error: false, data: results, message: "Data deleted"})
+            }else throw error;
+        })
+    }
+
+})
+
+Router.put('/', (req,res)=>{
+    console.log("Put")
+    let id = req.body.id;
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let role = req.body.role;
+    let salary = req.body.salary;
+
+    if(!id || !fname || !lname || !role || !salary){
+        return res.status(400).send({error: true , message: "Please enter the data"})
+    }
+
+    dbCon.query("UPDATE employee SET firstname = ?, lastname = ?, role = ?, salary = ? WHERE id = ?",
+    [fname, lname, role, salary, id], (error, results, field)=>{
+        if(!error){
+            return res.status(200).send({error: false, data: results, message: "Data Updated"})
+        } else throw error;
+    })
+
+})
+
+
 
 module.exports = Router
--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,4 @@ app.get("/test" , (req, res)=>{
 
 const PORT = process.env.PORT || 3000 ;
 app.listen(PORT , () => {console.log(`Server running on port ${PORT}`) } );
+module.exports = app;
